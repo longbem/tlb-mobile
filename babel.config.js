e7b3af35580337982b08a25fb0b8a229d3fc6008
Hoang Long <longcomputer9x@gmail.com>
2020-06-30 23:55:36 +0700

    config import

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@components': './src/components',
+          '@resources': './src/resources',
+          '@features': './src/features',
+          '@config': './src/config',
+          '@navigation': './src/navigation',
+        },
+      },
+    ],
+  ],
 };
