412b74100aa3c41446dbc22b1abd9b8df560e9d3
Hoang Long <longcomputer9x@gmail.com>
2020-07-04 10:01:46 +0700

    add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'no-debugger': 'error',
+    'no-duplicate-imports': 'error',
+    'no-unused-vars': 'error',
+    'no-console': 'warn',
+  },
 };
