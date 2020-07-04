0534415a901ada5e74dd1385b5befa4ceb171c12
Hoang Long <longcomputer9x@gmail.com>
2020-07-04 10:12:09 +0700

    test

--- src/features/home/index.js
+++ src/features/home/index.js
@@ -20,7 +20,7 @@ export default function Home() {
             marked: true,
             dotColor: 'red',
             activeOpacity: 0,
-            dotColor: 'red',
+            dotColor: 'reda',
           },
           '2020-07-04': { disabled: true, disableTouchEvent: true },
         }}
