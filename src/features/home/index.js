a8b37bbd8e24834b3e6de072a9a885ae9ddebb47
Hoang Long <longcomputer9x@gmail.com>
2020-07-04 09:54:58 +0700

    add packge

--- src/features/home/index.js
+++ src/features/home/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
+import { CalendarList } from 'react-native-calendars';
+import moment from 'moment';
 
 export default function Home() {
+  const [dateCurrent, setDateCurrent] = useState();
+
   return (
     <View>
       <Text>Home</Text>
+      <CalendarList
+        current={dateCurrent}
+        showScrollIndicator={false}
+        horizontal={true}
+        markedDates={{
+          '2020-07-01': { selected: true, marked: true, selectedColor: 'blue' },
+          '2020-07-02': { marked: true },
+          '2020-07-03': { marked: true, dotColor: 'red', activeOpacity: 0 },
+          '2020-07-04': { disabled: true, disableTouchEvent: true },
+        }}
+      />
     </View>
   );
 }
